668945f4832be343961aba2f0c81eadd9365a58a
Lucas <lucastkk@gmail.com>
2018-06-03 14:15:19 -0300

    Initial Commit

---
+++ IDDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IDDog
+//
+//  Created by Lucas Santos on 03/06/18.
+//  Copyright © 2018 Lucas Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
