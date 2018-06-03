68a11e479f6eef48c39766c7d35a7f4159abf2a6
Lucas <lucastkk@gmail.com>
2018-06-03 14:48:21 -0300

    initial commit

---
+++ IDDog/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  IDDog
+//
+//  Created by Lucas Santos on 03/06/18.
+//  Copyright © 2018 Lucas Santos. All rights reserved.
+//
+
+import Foundation
+
+struct User{
+    let email: String
+}
